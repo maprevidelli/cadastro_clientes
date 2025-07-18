dbd032591feb9afa355f0ce6fb40e39602b2d982
maprevidelli <marco.previdelli@gmail.com>
2025-07-18 20:41:04 -0300

    saída de consulta organizada

--- consulta_bd.py
+++ consulta_bd.py
@@ -2,11 +2,14 @@
 import pymongo
 import datetime
 from pprint import pprint
+
 cliente = pymongo.MongoClient("mongodb://localhost:27017/")
 # Cria o BD:
 db = cliente.softorama_database
 # Cria uma Collection no BD:
 cadastro_coll = db.cadastro
 
-for cadastros in cadastro_coll.find():
-    pprint(cadastros)
+for cadastro in cadastro_coll.find():
+    print("\nDocumento:")  # Indicador de novo documento
+    for chave, valor in cadastro.items():
+        print(f"{chave}: {valor}")
